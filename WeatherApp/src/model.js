072f9046c14c721ab76bb16841e671fbe58907fc
Дмитрий Бек <kutsia2011@mail.ru>
2018-12-10 02:34:53 +0300

    git: fix folder dublicate

--- WeatherApp/src/model.js
+++ WeatherApp/src/model.js
@@ -1,17 +1,12 @@
-function appModel() {
+function AppModel() {};
 
-    function searchWeather(searchStr) {
-        if (searchStr) {
-            const srsUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchStr}&units=metric&APPID=def1f3aa27892f05c5bc3dd1e69e4309`;
+AppModel.prototype.searchWeather = function(searchStr) {
+    if (searchStr) {
+        const srsUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchStr}&units=metric&APPID=def1f3aa27892f05c5bc3dd1e69e4309`;
 
-            return fetch(srsUrl)
-                .then(function (response) {
-                    return response.json();
-                })
-        } 
-    }
-    
-    return {
-        searchWeather: searchWeather
-    }
-}
+        return fetch(srsUrl)
+            .then(function (response) {
+                return response.json();
+            })
+    } 
+};
