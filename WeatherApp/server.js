e6de16c7cb2956010cfa67217c9c77031cfeec59
Дмитрий Бек <kutsia2011@mail.ru>
2018-12-13 23:42:08 +0300

    feat: implement backend part for city search

--- WeatherApp/server.js
+++ WeatherApp/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const failSistem = require('fs');
+const url = require('url');
 const contentType = {
     'html': 'text/html',
     'css': 'text/css',
@@ -7,10 +8,21 @@ const contentType = {
     'png': 'image/png',
     'jpg': 'image/jpeg',
     'woff2':'font/woof2'
-}
+};
+const cities = require('./data/cities.json');
 
 http.createServer((req, res) => {
-    const staticPath = req.url === '/' ? './index.html' : `.${req.url}`;
+    if (req.url.startsWith('/city-search')) {
+        const param = url.parse(req.url, true);
+        hendleCitySearch(param.query.q, res);
+    } else {
+        hendleStaticSources(req.url, res);
+    }
+})
+.listen(3000);
+
+function hendleStaticSources(src, res) {
+    const staticPath = src === '/' ? './index.html' : `.${src}`;
     
     failSistem.readFile(staticPath, (error, content) => {
         if (error || !content) {
@@ -20,14 +32,26 @@ http.createServer((req, res) => {
         }
 
         const pathParts = staticPath.split('.');
-        
+
         res.writeHead(200, {
             'Content-Type': contentType[pathParts[pathParts.length - 1]]
         });
-        res.end(data);  
+        res.end(content);  
     })
-})
-.listen(3000);
+};
+
+function hendleCitySearch(searchStr, res){
+    if (!searchStr){
+        res.end('Empty result');
+    }
+
+    const foundCities = cities.filter((item) => {
+        return item.city.toUpperCase().startsWith(searchStr.toUpperCase());
+    })
 
 
-
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    res.end(JSON.stringify(foundCities));  
+};
